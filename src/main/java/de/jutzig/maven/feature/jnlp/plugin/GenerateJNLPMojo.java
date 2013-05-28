0f3b0b8a3598884c4a97927ac770acdd8c576513
kai.zimmermann <kai.zimmermann@robotron.de>
2013-05-28 18:03:46 +0200

    neue Parameter * mit dem Parameter ressourceNamesWithoutVersion werden allen Dateien die Versionsnummern genommen * mit dem Parameter deleteFeatures werden die Features gelöscht

--- src/main/java/de/jutzig/maven/feature/jnlp/plugin/GenerateJNLPMojo.java
+++ src/main/java/de/jutzig/maven/feature/jnlp/plugin/GenerateJNLPMojo.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.text.MessageFormat;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -26,6 +30,7 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.osgi.framework.Version;
 import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -40,6 +45,11 @@ import org.xml.sax.SAXException;
  * @phase package
  */
 public class GenerateJNLPMojo extends AbstractMojo {
+
+	public static final String TYCHO_REPO_PATH = "target/site/";
+	public static final String PLUGINS_WITH_VERSION_JAR = "plugins/{0}_{1}.jar";
+	public static final String PLUGINS_WITHOUT_VERSION_JAR = "plugins/{0}.jar";
+
 	/**
 	 * Supported packaging.
 	 */
@@ -102,7 +112,21 @@ public class GenerateJNLPMojo extends AbstractMojo {
 	 */
 	private boolean isStandalone;
 
-	@Override
+	/**
+	 * @parameter default-value="false"
+	 */
+	protected boolean ressourceNamesWithoutVersion;
+
+	/**
+	 * @parameter default-value="false"
+	 */
+	protected boolean deleteFeatures;
+
+	/**
+	 * @parameter default-value="${basedir}"
+	 */
+	protected File baseDir;
+
 	public void execute() throws MojoExecutionException {
 		if (!ECLIPSE_FEATURE.equalsIgnoreCase(packaging)) {
 			getLog().debug(MessageFormat.format("The packaging of current project is not {0} but {1}", ECLIPSE_FEATURE, packaging));
@@ -112,11 +136,15 @@ public class GenerateJNLPMojo extends AbstractMojo {
 		Document feature = parseFeature(featureJar);
 		Document jnlpDoc = createJNLP(feature);
 		writeJNLP(featureJar, jnlpDoc);
+
+		if (deleteFeatures) {
+			featureJar.deleteOnExit();
+		}
 	}
 
 	private void writeJNLP(File featureJar, Document jnlpDoc) throws TransformerFactoryConfigurationError, MojoExecutionException {
 
-		File jnlp = new File(featureJar.getParentFile(), featureJar.getName().substring(0, featureJar.getName().length() - 3) + "jnlp");
+		File jnlp = new File(featureJar.getParentFile(), getJnlpFileName(featureJar.getName()));
 
 		Writer out = null;
 		try {
@@ -206,21 +234,29 @@ public class GenerateJNLPMojo extends AbstractMojo {
 
 				// if an arch is set, we must add several alternatives names for
 				// the same thing :-(
-				if (arch.equals("x86_64")) {
-					createResourceElement(plugin, getPlatformResourceGroup(platformSpecificResources, plugin, jnlp, jnlpElement, "x86_64"),
-							jnlp, jnlpElement);
-					createResourceElement(plugin, getPlatformResourceGroup(platformSpecificResources, plugin, jnlp, jnlpElement, "amd64"),
+				if (arch != null && arch.length() > 0) {
+					if ("x86_64".equals(arch)) {
+						createResourceElement(plugin,
+								getSpecificPlatformGroup(platformSpecificResources, jnlp, jnlpElement, "x86_64", os, ws), jnlp, jnlpElement);
+						createResourceElement(plugin,
+								getSpecificPlatformGroup(platformSpecificResources, jnlp, jnlpElement, "amd64", os, ws), jnlp, jnlpElement);
+
+					} else if ("x86".equals(arch)) {
+						createResourceElement(plugin,
+								getSpecificPlatformGroup(platformSpecificResources, jnlp, jnlpElement, "x86", os, ws), jnlp, jnlpElement);
+						createResourceElement(plugin,
+								getSpecificPlatformGroup(platformSpecificResources, jnlp, jnlpElement, "i386", os, ws), jnlp, jnlpElement);
+						createResourceElement(plugin,
+								getSpecificPlatformGroup(platformSpecificResources, jnlp, jnlpElement, "i686", os, ws), jnlp, jnlpElement);
+					}
+
+				} else if ((os != null && os.length() > 0) || (ws != null && ws.length() > 0)) {
+					createResourceElement(plugin, getSpecificPlatformGroup(platformSpecificResources, jnlp, jnlpElement, arch, os, ws),
 							jnlp, jnlpElement);
 
-				} else if (arch.equals("x86")) {
-					createResourceElement(plugin, getPlatformResourceGroup(platformSpecificResources, plugin, jnlp, jnlpElement, "x86"),
-							jnlp, jnlpElement);
-					createResourceElement(plugin, getPlatformResourceGroup(platformSpecificResources, plugin, jnlp, jnlpElement, "i386"),
-							jnlp, jnlpElement);
-					createResourceElement(plugin, getPlatformResourceGroup(platformSpecificResources, plugin, jnlp, jnlpElement, "i686"),
-							jnlp, jnlpElement);
-				} else
+				} else {
 					createResourceElement(plugin, jarResources, jnlp, jnlpElement);
+				}
 			}
 
 			return jnlp;
@@ -232,12 +268,28 @@ public class GenerateJNLPMojo extends AbstractMojo {
 		}
 	}
 
-	private Element getPlatformResourceGroup(Map<String, Element> platformSpecificResources, Element plugin, Document jnlp, Element root,
-			String arch) {
-		String os = plugin.getAttribute("os");
+	private Element getSpecificPlatformGroup(Map<String, Element> platformSpecificGroups, Document jnlp, Element root, String arch,
+			String os, String ws) {
 
-		String key = os + arch;
-		Element resourceGroup = platformSpecificResources.get(key);
+		String archKey = "arch=";
+		String osKey = ";os=";
+		String wsKey = ";ws=";
+
+		if (arch != null) {
+			archKey = archKey + arch;
+		}
+
+		if (os != null) {
+			osKey = osKey + os;
+		}
+
+		if (ws != null) {
+			wsKey = wsKey + ws;
+		}
+
+		String key = archKey + osKey + wsKey;
+		Element resourceGroup = platformSpecificGroups.get(key);
+
 		if (resourceGroup == null) {
 			resourceGroup = jnlp.createElement("resources");
 			try {
@@ -245,16 +297,21 @@ public class GenerateJNLPMojo extends AbstractMojo {
 			} catch (DOMException e) {
 				System.out.println("Error processing DOM");
 			}
-			platformSpecificResources.put(key, resourceGroup);
 
-			if (os.contains("win"))
+			platformSpecificGroups.put(key, resourceGroup);
+
+			if (os.contains("win")) {
 				resourceGroup.setAttribute("os", "Windows");
-			if (os.contains("mac"))
+			} else if (os.contains("mac")) {
 				resourceGroup.setAttribute("os", "Mac");
-			if (os.contains("linux"))
+			} else if (os.contains("linux")) {
 				resourceGroup.setAttribute("os", "Linux");
+			} else {
+				resourceGroup.setAttribute("os", os);
+			}
 
 			resourceGroup.setAttribute("arch", arch);
+			// resourceGroup.setAttribute("ws", ws);
 		}
 
 		return resourceGroup;
@@ -285,11 +342,15 @@ public class GenerateJNLPMojo extends AbstractMojo {
 
 				Element extensionResources = targetJnlp.createElement("extension");
 				String featureName = includedEclipseFeature.getName();
-				extensionResources.setAttribute("href", "features/" + featureName.substring(0, featureName.length() - 4) + ".jnlp");
+				extensionResources.setAttribute("href", "features/" + getJnlpFileName(featureName));
 				try {
 					targetJnlpResourcesGroup.appendChild(extensionResources);
 				} catch (DOMException e) {
 					System.out.println("Error processing DOM");
+				}
+
+				if (deleteFeatures) {
+					includedEclipseFeature.deleteOnExit();
 				}
 
 			} else {
@@ -298,11 +359,14 @@ public class GenerateJNLPMojo extends AbstractMojo {
 		}
 	}
 
-	private void createResourceElement(Element plugin, Element resource, Document jnlp, Node jnlpElement) {
+	private void createResourceElement(Element plugin, Element resource, Document jnlp, Node jnlpElement) throws MojoExecutionException {
 		String version = plugin.getAttribute("version");
-		if (version == null || version.length() == 0 || version.equals("0.0.0"))
+		if (version == null || version.length() == 0 || version.equals("0.0.0")) {
 			// this plugin is not included for some reason. We must skip
 			return;
+		}
+
+		String id = plugin.getAttribute("id");
 
 		Element jar = jnlp.createElement("jar");
 		try {
@@ -310,8 +374,98 @@ public class GenerateJNLPMojo extends AbstractMojo {
 		} catch (DOMException e) {
 			System.out.println("Error processing DOM");
 		}
-		jar.setAttribute("href", "plugins/" + plugin.getAttribute("id") + "_" + plugin.getAttribute("version") + ".jar");
 
+		String bundleNameWithVersion = getPluginPath(false, id, version);
+		String bundleName = getPluginPath(id, version);
+
+		if (ressourceNamesWithoutVersion) {
+			renamePlugin(id, bundleNameWithVersion, bundleName);
+		}
+
+		jar.setAttribute("href", bundleName);
+	}
+
+	private void renamePlugin(String id, String bundleNameWithVersion, String bundleName) throws MojoExecutionException {
+		File bundle = new File(baseDir, TYCHO_REPO_PATH + bundleNameWithVersion);
+		if (bundle.exists()) {
+
+			File newBundleWithoutVersion = new File(baseDir, TYCHO_REPO_PATH + bundleName);
+			boolean renamed = bundle.renameTo(newBundleWithoutVersion);
+
+			if (!renamed) {
+				String bsn = id;
+
+				if (areThereMultipleBundles(bundle.getParentFile(), bsn)) {
+					getLog().info("plugin " + bundleName + " exist in more than one version. All but the last will be removed!");
+					removeAllButTheGreatest(bundle.getParentFile(), bsn);
+
+					if (bundle.exists()) {
+						bundle.renameTo(newBundleWithoutVersion);
+					}
+
+				} else {
+					throw new MojoExecutionException(MessageFormat.format(
+							"renaming {0} to {1} was not successfull (there are no other bundles with the same bsn)!",
+							bundle.getAbsolutePath(), newBundleWithoutVersion));
+				}
+			}
+		} else {
+			getLog().debug(bundle.getAbsolutePath() + " could not renamed, because it didn't exist (maybe it was renamed already)!");
+		}
+	}
+
+	private void removeAllButTheGreatest(File pluginsDir, final String bsn) throws MojoExecutionException {
+		String[] bundlesWithSameBSN = pluginsDir.list(new BundleSymbolicNameFileFilter(bsn));
+		Map<Version, File> versionedFiles = new HashMap<Version, File>();
+		Version maxVersion = null;
+
+		for (String fileName : bundlesWithSameBSN) {
+			File file = new File(pluginsDir, fileName);
+
+			try {
+				JarFile jar = new JarFile(file);
+				Attributes attributes = jar.getManifest().getMainAttributes();
+				Version version = new Version(attributes.getValue("Bundle-Version"));
+				jar.close();
+
+				if (maxVersion == null || maxVersion.compareTo(version) < 0) {
+					maxVersion = version;
+				} else {
+					File secondBundleWithSameVersion = versionedFiles.get(version);
+					if (secondBundleWithSameVersion != null) {
+						secondBundleWithSameVersion.delete();
+					}
+				}
+				versionedFiles.put(version, file);
+
+			} catch (IOException e) {
+				throw new MojoExecutionException("one of the bundles with bsn " + bsn + " could no opened!", e);
+			}
+		}
+
+		for (Iterator<Version> iterator = versionedFiles.keySet().iterator(); iterator.hasNext();) {
+			Version version = (Version) iterator.next();
+			if (!version.equals(maxVersion)) {
+				versionedFiles.get(version).delete();
+			}
+		}
+	}
+
+	private boolean areThereMultipleBundles(File pluginsDir, final String bsn) {
+		String[] bundlesWithSameBSN = pluginsDir.list(new BundleSymbolicNameFileFilter(bsn));
+		return bundlesWithSameBSN.length > 1;
+	}
+
+	protected String getPluginPath(String id, String version) {
+		return getPluginPath(ressourceNamesWithoutVersion, id, version);
+	}
+
+	protected String getPluginPath(boolean withoutVersion, String id, String version) {
+		String naming = PLUGINS_WITH_VERSION_JAR;
+		if (withoutVersion) {
+			naming = PLUGINS_WITHOUT_VERSION_JAR;
+		}
+		return MessageFormat.format(naming, id, version);
 	}
 
 	private Document parseFeature(File featureJar) {
@@ -351,4 +505,42 @@ public class GenerateJNLPMojo extends AbstractMojo {
 		}
 		return null;
 	}
+
+	protected String removeVersion(String filename) {
+		if (filename == null || filename.length() == 0) {
+			return filename;
+		}
+
+		int underScore = filename.indexOf('_');
+		if (underScore == -1) {
+			return filename;
+		}
+
+		int extension = filename.lastIndexOf('.');
+
+		return filename.substring(0, underScore).concat(filename.substring(extension));
+	}
+
+	protected String getJnlpFileName(String featureFilename) {
+		if (ressourceNamesWithoutVersion) {
+			featureFilename = removeVersion(featureFilename);
+		}
+
+		int extension = featureFilename.lastIndexOf('.');
+
+		return featureFilename.substring(0, extension) + ".jnlp";
+	}
+
+	private final class BundleSymbolicNameFileFilter implements FilenameFilter {
+		private final String bsn;
+
+		private BundleSymbolicNameFileFilter(String bsn) {
+			this.bsn = bsn;
+		}
+
+		public boolean accept(File dir, String name) {
+			return name.startsWith(bsn);
+		}
+	}
+
 }
---
+++ src/test/java/de/jutzig/maven/feature/jnlp/plugin/VersionNumberTests.java
@@ -0,0 +1,65 @@
+package de.jutzig.maven.feature.jnlp.plugin;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Der Testfall überprüft, ob das Plugin die Versionnummern aus den Namen der
+ * Libs und Features schneiden kann.
+ */
+@RunWith(value = Parameterized.class)
+public class VersionNumberTests extends GenerateJNLPMojo {
+	private static final List<String[]> testCases = new ArrayList<String[]>();
+	private String expectedFilename;
+	private String givenFilename;
+	private String expectedJnlp;
+
+	static {
+		testCases.addAll(Arrays.asList(new String[][] {
+				{ "javax.wsdl_1.6.2.v201012040545.jar", "javax.wsdl.jar", "javax.wsdl.jnlp" },
+				{ "org.eclipse.core.databinding.property_1.4.100.v20120523-1955.jar", "org.eclipse.core.databinding.property.jar",
+						"org.eclipse.core.databinding.property.jnlp" },
+				{ "org.junit_4.10.0.v4_10_0_v20120426-0900.jar", "org.junit.jar", "org.junit.jnlp" } }));
+	}
+
+	@Parameters
+	public static List<String[]> testingData() {
+		return testCases;
+	}
+
+	public VersionNumberTests(String givenFilename, String expectedFilename, String expectedJnlp) {
+		this.expectedFilename = expectedFilename;
+		this.givenFilename = givenFilename;
+		this.expectedJnlp = expectedJnlp;
+	}
+
+	@Test
+	public void testSimpleFilename() {
+		assertEquals(expectedFilename, removeVersion(givenFilename));
+	}
+
+	@Test
+	public void testPluginWithVersion() {
+		assertEquals("plugins/abc_1.0.0.jar", getPluginPath("abc", "1.0.0"));
+	}
+
+	@Test
+	public void testPluginWithoutVersion() {
+		this.ressourceNamesWithoutVersion = true;
+		assertEquals("plugins/abc.jar", getPluginPath("abc", "1.0.0"));
+	}
+
+	@Test
+	public void testJnlpNaming() {
+		this.ressourceNamesWithoutVersion = true;
+		assertEquals(expectedJnlp, getJnlpFileName(givenFilename));
+	}
+}
